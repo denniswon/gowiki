5a86495b1572094c448c83ae8342d47dd4a78658
Jong Hyuck Won <jhwon0820@gmail.com>
2021-07-05 12:25:00 -0700

    basic websocket integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,23 +2,22 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+import { connect, sendMsg } from './api'
+
+type Props = {
+}
+
+const App: React.FunctionComponent<Props> = (props: Props) => {
+  connect()
+
+  const send = () => {
+    console.log('hello')
+    sendMsg('hello')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={send}>Hit</button>
     </div>
   )
 }
