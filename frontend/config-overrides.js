bc54455d47bf0700cb6d0733aaa7fff6e9d9b0a7
Jong Hyuck Won <jhwon0820@gmail.com>
2021-07-09 14:37:43 -0700

    eject CRA

--- frontend/config-overrides.js
+++
@@ -1,9 +0,0 @@
-// config-overrides.js
-const { alias, configPaths } = require('react-app-rewire-alias')
-
-module.exports = function override(config) {
-  alias({
-    ...configPaths('tsconfig.paths.json'),
-  })(config)
-  return config
-}
